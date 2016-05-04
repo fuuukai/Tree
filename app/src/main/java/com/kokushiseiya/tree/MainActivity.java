b60ca694396daaa336348c96b5f142974eebaf75
fukai <caxa8.fukai@gmail.com>
2016-05-04 14:34:47 +0900

    sample could be run

--- app/src/main/java/com/kokushiseiya/tree/MainActivity.java
+++ app/src/main/java/com/kokushiseiya/tree/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kokushiseiya.tree;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,15 +12,12 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
